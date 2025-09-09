d2dfd48ba50834f2986557b264bd839d45c08600
Javier MGB <72402925+JavierMGB@users.noreply.github.com>
2025-09-09 21:33:05 +0000

    Tienda

--- que.py
+++ que.py
@@ -1,1 +1,16 @@
+import random
+
+class Tienda():
+    def __init__(self,productos,trabajadores,ganancias):
+        self.productos = productos
+        self.trabajadores = trabajadores
+        self.ganancias = ganancias
+
+    
+Hola = random.seed(float, 2)
+
+print(Hola)
+
+
+
 
