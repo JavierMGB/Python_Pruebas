9a444673652f34e3b73d5eac38b03fcd62e1ddbb
Javier MGB <72402925+JavierMGB@users.noreply.github.com>
2025-09-16 17:02:10 +0100

    Update que.py

--- que.py
+++ que.py
@@ -1,20 +1,29 @@
-import random
-
 class Tienda():
     def __init__(self,productos,trabajadores,ganancias):
         self.productos = productos
         self.trabajadores = trabajadores
         self.ganancias = ganancias
 
+    def info(self):
+        return f"Hay {self.trabajadores} trabajadores y quedan {self.productos} productos"
+
+print("Cree una tienda")
+productos = (input("Ingrese la cantidad de productos: "))
+trabajadores = (input("Ingrese la cantidad de trabajadores: "))  
+
+ganancia = (input("Agregar ganancia: "))
+ganancia = int(ganancia)
+
+while ganancia <= 0:
+    print("No se ha podido registrar la ganancia")
+    (input("Agregar ganancia: "))
+    ganancia = int(ganancia)
+    break
+
+if ganancia > 0:
+    print("Ganancia registrada con exito")
     
-Hola = random.randrange(2, 100)
 
-print(Hola)
+Tienda = Tienda(productos,trabajadores,ganancia)
 
-
-
-print(Hola)
-
-
-
-
+print(Tienda.info())
